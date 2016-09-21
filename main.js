0810448def013b9d719f57e99854ee780fe78e23
Sven Haberer <svenhaberer@incub8.de>
2016-09-21 23:24:50 +0200

    Added rudimentary code for room upgrading.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ module.exports.loop = function() {
         }
     }
 
+    var creepNumber = 0;
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.carry.energy < creep.carryCapacity) {
@@ -16,18 +17,28 @@ module.exports.loop = function() {
             }
         }
         else {
-            var target = creep.pos.findClosestByPath(
-                FIND_STRUCTURES, {
-                    filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
-                    structure.energy < structure.energyCapacity
-                });
+            var target;
+            if (creepNumber % 5 == 0) {
+                target = creep.pos.findClosestByPath(
+                    FIND_STRUCTURES, {
+                        filter: (structure) => structure.structureType == STRUCTURE_CONTROLLER
+                    });
+            }
+            else {
+                target = creep.pos.findClosestByPath(
+                    FIND_STRUCTURES, {
+                        filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                        structure.energy < structure.energyCapacity
+                    });
+            }
             if (target) {
                 if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                 }
             }
         }
+        creepNumber++;
     }
 
     for (var name in Game.spawns) {
