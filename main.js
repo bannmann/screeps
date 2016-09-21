4cf12d00cb427d7c44541daa2db9903b72e206cc
Sven Haberer <svenhaberer@incub8.de>
2016-09-21 22:38:05 +0200

    Added simple harvesting script.

---
+++ main.js
@@ -0,0 +1,26 @@
+module.exports.loop = function() {
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if (creep.carry.energy < creep.carryCapacity) {
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (source) {
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }
+        }
+        else {
+            var target = creep.pos.findClosestByPath(
+                FIND_STRUCTURES, {
+                    filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                    structure.energy < structure.energyCapacity
+                });
+            if (target) {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        }
+    }
+};
