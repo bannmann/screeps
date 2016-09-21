50c2017a6397332537fc0be5b5581533ef20ab87
Sven Haberer <svenhaberer@incub8.de>
2016-09-21 23:38:59 +0200

    Creeps now have intents.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var INTENT = {
+    HARVEST_ENERGY: "HARVEST_ENERGY",
+    TRANSFER_TO_MY_STRUCTURE: "TRANSFER_TO_MY_STRUCTURE",
+    TRANSFER_TO_CONTROLLER: "TRANSFER_TO_CONTROLLER"
+};
+
 module.exports.loop = function() {
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
@@ -8,41 +14,62 @@ module.exports.loop = function() {
     var creepNumber = 0;
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if (creep.carry.energy < creep.carryCapacity) {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if (source) {
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source);
+
+        var target;
+        switch (creep.memory.intent) {
+            case INTENT.HARVEST_ENERGY:
+                target = creep.pos.findClosestByPath(FIND_SOURCES);
+                if (target) {
+                    if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                    else if (creep.carry.energy == creep.carryCapacity) {
+                        if (creepNumber % 5 == 0) {
+                            creep.memory.intent = INTENT.TRANSFER_TO_CONTROLLER;
+                        }
+                        else {
+                            creep.memory.intent = INTENT.TRANSFER_TO_MY_STRUCTURE;
+                        }
+                    }
                 }
-            }
-        }
-        else {
-            var target;
-            if (creepNumber % 5 == 0) {
+                break;
+            case INTENT.TRANSFER_TO_CONTROLLER:
                 target = creep.pos.findClosestByPath(
                     FIND_STRUCTURES, {
                         filter: (structure) => structure.structureType == STRUCTURE_CONTROLLER
                     });
-            }
-            else {
+                if (target) {
+                    if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                    else if (creep.carry.energy == 0) {
+                        creep.memory.intent = INTENT.HARVEST_ENERGY;
+                    }
+                }
+                break;
+            case INTENT.TRANSFER_TO_MY_STRUCTURE:
                 target = creep.pos.findClosestByPath(
                     FIND_STRUCTURES, {
                         filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
                         structure.energy < structure.energyCapacity
                     });
-            }
-            if (target) {
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
+                if (target) {
+                    if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                    else if (creep.carry.energy == 0) {
+                        creep.memory.intent = INTENT.HARVEST_ENERGY;
+                    }
                 }
-            }
+                break;
         }
+
         creepNumber++;
     }
 
     for (var name in Game.spawns) {
         var spawn = Game.spawns[name];
-        spawn.createCreep([WORK, MOVE, CARRY]);
+        spawn.createCreep([WORK, MOVE, CARRY], undefined, {intent: INTENT.HARVEST_ENERGY});
     }
 };
