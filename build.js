2923067e1c42516238475073d90f75638605f1e4
Sven Haberer <svenhaberer@incub8.de>
2016-09-23 14:05:13 +0200

    Added build intent.

---
+++ build.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "build", ponder: function(creep) {
+        if (creep.carry.energy == creep.carryCapacity) {
+            var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            if (target) {
+                creep.memory.intent = this.name;
+                creep.memory.target = target.id;
+            }
+        }
+    }, pursue: function(creep) {
+        var target = Game.getObjectById(creep.memory.target);
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+        }
+        if (target.progress == target.progressTotal || creep.carry.energy == 0) {
+            delete creep.memory.intent;
+            delete creep.memory.target;
+        }
+    }
+};
--- intents.js
+++ intents.js
@@ -1,9 +1,11 @@
+var build = require("build");
 var harvestEnergy = require("harvestEnergy");
 var transferToMyStructure = require("transferToMyStructure");
 var transferToController = require("transferToController");
 var idle = require("idle");
 
 module.exports = {
+    build: build,
     harvestEnergy: harvestEnergy,
     transferToMyStructure: transferToMyStructure,
     transferToController: transferToController,
