002c55d3d05a034a286e2ebe187ba3967e8087c8
Sven Haberer <svenhaberer@incub8.de>
2016-09-24 21:29:07 +0200

    Added automatic safe mode activation.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 require("prototypes")();
+var safeModeManager = require("safeModeManager");
 var memoryCleaner = require("memoryCleaner");
 var intentManager = require("intentManager");
 var spawnManager = require("spawnManager");
 
 module.exports.loop = function() {
+    safeModeManager.manage();
     memoryCleaner.clean();
     intentManager.processIntents();
     spawnManager.spawnCreepIfNecessary();
---
+++ safeModeManager.js
@@ -0,0 +1,15 @@
+module.exports.manage = function() {
+    for (var name in Game.rooms) {
+        var room = Game.rooms[name];
+        var controller = room.controller;
+        if (controller.my &&
+            !controller.safeMode &&
+            !controller.safeModeCooldown &&
+            controller.safeModeAvailable >
+            0 &&
+            room.find(FIND_HOSTILE_CREEPS).length >
+            0) {
+            controller.activateSafeMode();
+        }
+    }
+};
