ff49257ac5f5219ee605bef038f6a62a1cdf6bc3
Sven Haberer <svenhaberer@incub8.de>
2016-09-24 23:16:34 +0200

    spawnManager now spawns bigger creeps according to energy capacity in the room.

--- spawnManager.js
+++ spawnManager.js
@@ -3,7 +3,21 @@ module.exports.spawnCreepIfNecessary = function() {
     if (creepCount < 20) {
         for (var name in Game.spawns) {
             var spawn = Game.spawns[name];
-            spawn.createCreep([WORK, MOVE, MOVE, CARRY]);
+            var creepSize = Math.floor(spawn.room.energyCapacityAvailable / 250);
+            if (spawn.room.energyAvailable >= creepSize * 250) {
+                var body = [];
+                for (var i = 0; i < creepSize; i++) {
+                    body.push(WORK);
+                }
+                for (var i = 0; i < creepSize; i++) {
+                    body.push(CARRY);
+                }
+                for (var i = 0; i < creepSize; i++) {
+                    body.push(MOVE);
+                    body.push(MOVE);
+                }
+                spawn.createCreep(body);
+            }
         }
     }
 };
