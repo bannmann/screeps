6c8bda3d0d5d4bf89e8eb8e0089755b856a9a95c
Jens Bannmann <jens.b@web.de>
2016-11-05 08:55:01 +0100

    Tweaked debugging code.
    - Added by dacac4b26bf14384e43e0cce06467783b26acb0b.

--- action_move.js
+++ action_move.js
@@ -55,6 +55,8 @@ module.exports = {
     isMovementNeeded(creep, path, intent) {
         if (path.length > 1 && !path[0]) {
             Game.notify("isMovementNeeded: path is ***" + JSON.stringify(path) + "***, intent=" + intent + ", movementStatus ***" + JSON.stringify(creep.memory.movementStatus) + "***");
+            // Need to return here as the game doesn't seem to send my notifications when it encounters an error.
+            return false;
         }
         return path.length > 1 || !creep.pos.inRangeTo(path[0].x, path[0].y, intent.range);
     },
