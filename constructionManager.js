6a47bc83608bdee1b3939692b365b911f156731b
Jens Bannmann <jens.b@web.de>
2016-12-04 00:00:38 +0100

    Only attempt to create spawns in my own rooms.

--- constructionManager.js
+++ constructionManager.js
@@ -1,7 +1,7 @@
 module.exports.manage = function() {
     _.each(Game.flags,
         (flag)=> {
-            if (flag.name.startsWith("spawnLocation") && flag.room) {
+            if (flag.name.startsWith("spawnLocation") && flag.room && flag.room.controller && flag.room.controller.my) {
                 flag.room.createConstructionSite(flag.pos, STRUCTURE_SPAWN);
                 flag.remove();
             }
