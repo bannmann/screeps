e58f912064dd09bb2e78ea4cca1b3ab92c52fbba
Jens Bannmann <jens.b@web.de>
2016-12-06 23:48:48 +0100

    Bugfix: moveAction again gracefully deals with old statuses.

--- action_move.js
+++ action_move.js
@@ -74,7 +74,7 @@ module.exports = {
     perform: function(creep) {
         var status = this.getStatus(creep);
 
-        if (status.lastRoom != creep.pos.roomName) {
+        if (!this.isLegacyStatus(status) && status.lastRoom != creep.pos.roomName) {
             // The creep just switched rooms and is now located on the new room's exit. Leave it to avoid oscillating.
             creep.logDebug("entering " + creep.pos.roomName + " to " + creep.memory.intent);
 
