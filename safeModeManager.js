88e9a451ee07690f3d6a4ee2b0f9d158e6b5207c
Jens Bannmann <jens.b@web.de>
2016-12-07 21:36:25 +0100

    Bugfix: safeModeManager no longer chokes on rooms without a controller.

--- safeModeManager.js
+++ safeModeManager.js
@@ -2,7 +2,8 @@ module.exports.manage = function() {
     for (var name in Game.rooms) {
         var room = Game.rooms[name];
         var controller = room.controller;
-        if (controller.my &&
+        if (controller &&
+            controller.my &&
             !controller.safeMode &&
             !controller.safeModeCooldown &&
             controller.safeModeAvailable >
