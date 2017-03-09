bannmann/screeps

commit 7cceb3905fbd152fff4f713945e1e7d48f44dd13

Added intentsUtil.finish() and abort() as more specialized versions of reset().
- For many intents, this avoids waiting an extra turn before resetting the creep.
- Replaced many intentsUtil.reset() calls with semantic abort().
- Most intents did not previously log unexpected result codes.

intent_harvestEnergy.js
@@ -56,16 +56,20 @@ module.exports = {
 
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
-        if (creep.carry.energy == creep.carryCapacity || target == null || target.energy == 0) {
+        if (target == null) {
+            intentsUtil.abort(creep, this, "missing target " + creep.memory.target);
+        }
+        else if (target.energy == 0) {
+            intentsUtil.abort(creep, this, "empty target " + creep.memory.target);
+        }
+        else if (creep.carry.energy == creep.carryCapacity) {
             intentsUtil.reset(creep);
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.harvest(target) != OK) {
-                intentsUtil.reset(creep);
-            }
+            intentsUtil.finish(creep, this, creep.harvest(target));
         }
     }
 };

intent_build.js
@@ -35,16 +35,17 @@ module.exports = {
     },
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
-        if (!target || creep.carry.energy == 0) {
-            intentsUtil.reset(creep);
+        if (!target) {
+            intentsUtil.abort(creep, this, "missing target " + creep.memory.target);
+        }
+        else if (creep.carry.energy == 0) {
+            intentsUtil.abort(creep, this, "no energy");
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.build(target) != OK) {
-                intentsUtil.reset(creep);
-            }
+            intentsUtil.finish(creep, this, creep.build(target));
         }
     }
 };

intent_chargeController.js
@@ -37,15 +37,13 @@ module.exports = {
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
         if (creep.carry.energy == 0) {
-            intentsUtil.reset(creep);
+            intentsUtil.abort(creep, this, "no energy");
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.upgradeController(target) != OK) {
-                intentsUtil.reset(creep);
-            }
+            intentsUtil.finish(creep, this, creep.upgradeController(target));
         }
     }
 };

intent_chargeSpawn.js
@@ -47,19 +47,20 @@ module.exports = {
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
         if (!target) {
-            intentsUtil.reset(creep);
+            intentsUtil.abort(creep, this, "missing target " + creep.memory.target);
         }
-        else if (creep.carry.energy == 0 || target.energy == target.energyCapacity) {
-            target.deregisterEnergyTransaction(creep);
-            intentsUtil.reset(creep);
+        else if (creep.carry.energy == 0) {
+            intentsUtil.abort(creep, this, "no energy");
+        }
+        else if (target.energy == target.energyCapacity) {
+            intentsUtil.abort(creep, this, "target " + creep.memory.target + " being full");
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.transfer(target, RESOURCE_ENERGY) != OK) {
-                intentsUtil.reset(creep);
-            }
+            target.deregisterEnergyTransaction(creep);
+            intentsUtil.finish(creep, this, creep.transfer(target, RESOURCE_ENERGY));
         }
     }
 };

intent_chargeTower.js
@@ -37,17 +37,18 @@ module.exports = {
     },
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
-        if (creep.carry.energy == 0 || target.energy == target.energyCapacity) {
-            target.deregisterEnergyTransaction(creep);
-            intentsUtil.reset(creep);
+        if (creep.carry.energy == 0) {
+            intentsUtil.abort(creep, this, "no energy");
+        }
+        else if (target.energy == target.energyCapacity) {
+            intentsUtil.abort(creep, this, "target " + creep.memory.target + " being full");
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.transfer(target, RESOURCE_ENERGY) != OK) {
-                intentsUtil.reset(creep);
-            }
+            target.deregisterEnergyTransaction(creep);
+            intentsUtil.finish(creep, this, creep.transfer(target, RESOURCE_ENERGY));
         }
     }
 };

intent_claimRoom.js
@@ -30,22 +30,17 @@ module.exports = {
     },
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
-        if (target.my) {
-            intentsUtil.reset(creep);
-
-            _.each(Game.flags,
-                (flag)=> {
-                    if (flag.name.startsWith("claim")) {
-                        flag.remove();
-                    }
-                });
-        }
-        else if (moveAction.isActive(creep)) {
+        if (moveAction.isActive(creep)) {
             moveAction.perform(creep, this);
         }
         else {
-            if (creep.claimController(target) != OK) {
-                intentsUtil.reset(creep);
+            if (intentsUtil.finish(creep, this, creep.claimController(target))) {
+                _.each(Game.flags,
+                    (flag)=> {
+                        if (flag.name.startsWith("claim")) {
+                            flag.remove();
+                        }
+                    });
             }
         }
     }

intent_meleeAttack.js
@@ -61,11 +61,11 @@ module.exports = {
             intentsUtil.reset(creep);
         }
         else {
-            var result = creep.attack(target);
-            if (result == ERR_NOT_IN_RANGE) {
+            var attackResult = creep.attack(target);
+            if (attackResult == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
-            } else if (result != OK) {
-                intentsUtil.reset(creep);
+            } else if (attackResult != OK) {
+                intentsUtil.abort(creep, this, "attackResult " + attackResult);
             }
         }
     }

intent_pickupEnergy.js
@@ -42,16 +42,14 @@ module.exports = {
     },
     pursue: function(creep) {
         var target = Game.getObjectById(creep.memory.target);
-        if (!target || creep.carry.energy == creep.carryCapacity) {
-            intentsUtil.reset(creep);
+        if (!target) {
+            intentsUtil.abort(creep, this, "missing target " + creep.memory.target);
         }
         else if (moveAction.isActive(creep)) {
             moveAction.perform(creep);
         }
         else {
-            if (creep.pickup(target) != OK) {
-                intentsUtil.reset(creep);
-            }
+            intentsUtil.finish(creep, this, creep.pickup(target));
         }
     }
 };

commit 2970c0d57635ae1f520a102ebb45b5fb40287b87

Bugfix: harvestEnergy finished after harvesting once
- Regression caused by 7cceb39

intent_harvestEnergy.js
@@ -69,7 +69,10 @@ module.exports = {
             moveAction.perform(creep);
         }
         else {
-            intentsUtil.finish(creep, this, creep.harvest(target));
+            var harvestResult = creep.harvest(target);
+            if (harvestResult != OK) {
+                intentsUtil.abort(creep, this, harvestResult);
+            }
         }
     }
 };