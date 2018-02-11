5b127b8543750e4173c7ca16d560ff5e2046c423
Jens Bannmann <jens.b@web.de>
2018-02-11 12:29:36 +0100

    Gruntfile now supports rsync goal for local server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     var config = require('./.screeps.json');
 
     grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-rsync');
 
     grunt.initConfig({
         screeps: {
@@ -20,6 +21,20 @@ module.exports = function(grunt) {
                         ]
                     }
                 ]
+            }
+        },
+
+        rsync: {
+            options: {
+                args: ["--verbose", "--checksum"],
+                exclude: [".git*"],
+                recursive: false
+            },
+            private: {
+                options: {
+                    src: '*',
+                    dest: config.private_directory
+                }
             }
         }
     });
